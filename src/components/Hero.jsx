ec7bcb7e712143d68a423e4ba020e6e58f201922
Ali <nurhalishasari@gmail.com>
2025-02-18 20:30:12 +0800

    first-commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Hero.css";
+
+const ProfilePage = () => {
+  return (
+    <div className="hero-container">
+      <div className="hero-card">
+        <div className="hero-logo">NS</div>
+        <h1 className="hero-title">
+          Hello, I am{" "}
+          <span className="circle-wrapper">
+            <span className="circle-text">Nur</span>
+            <div className="circle-animation"></div>
+          </span>
+          !
+        </h1>
+        <p className="hero-subtitle">Web Developer | Designer</p>
+        <p className="hero-text">
+          I believe brevity is key. My print and digital designs are all about minimalism and elegance.
+        </p>
+        <div>
+          <button className="hero-button">Hire Me</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
