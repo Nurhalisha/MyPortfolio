d81211bacb958c7612d9bcc8520caca969127857
Ali <nurhalishasari@gmail.com>
2025-05-05 04:41:22 +0800

    update

--- src/assets/components/Blog.jsx
+++ src/assets/components/Blog.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./blog.css";
+import "./Blog.css";
 import bgImage from "../image/BGRND.jpg";
 import collageImage from "../image/collage.jpg";
 
